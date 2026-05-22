7176211aa4317e6e52f5951ed1e8c2fe39ebfe85
ayush <ayushjaiswal12123@gmail.com>
2026-05-22 08:53:37 +0530

    fixed message issue

--- server.js
+++ server.js
@@ -15,42 +15,49 @@ io.on("connection", (socket) => {
   // Handle user joining a room
   socket.on("join_room", (username, room) => {
     userName = username;
-    socket.leave(currentRoom); 
+    if (currentRoom) {
+      socket.leave(currentRoom); 
+      socket.to(currentRoom).emit("message", { type: "system", text: `${userName} has left the room.` });
+    }
     socket.join(room);
     currentRoom = room;
-    io.to(room).emit("message", `${username} has joined the room: ${room}`); 
+    
+    socket.emit("message", { type: "system", text: `Welcome to ${room}!` });
+    socket.to(room).emit("message", { type: "system", text: `${username} has joined the room.` }); 
     io.emit("update_rooms", getRoomList());
   });
 
   // Handle user sending a message to the room
   socket.on("message", (msg, room) => {
     if (room) {
-      io.to(room).emit("message", `${userName}: ${msg}`);
+      // Send only to others in the room
+      socket.to(room).emit("message", { type: "chat", sender: userName, text: msg });
     }
   });
 
   // Handle user disconnecting
   socket.on("disconnect", () => {
-    io.to(currentRoom).emit("message", `${userName} has left the room.`);
+    if (currentRoom && userName) {
+      socket.to(currentRoom).emit("message", { type: "system", text: `${userName} has left the room.` });
+    }
     io.emit("update_rooms", getRoomList());
   });
 
   // Helper function to get the list of rooms
   const getRoomList = () => {
-  const rooms = {};
-  const socketIds = new Set([...io.sockets.sockets.keys()]);
+    const rooms = {};
+    const socketIds = new Set([...io.sockets.sockets.keys()]);
 
-  for (const [room, socketSet] of io.sockets.adapter.rooms) {
-    if (!socketIds.has(room)) { 
-      rooms[room] = socketSet.size;
+    for (const [room, socketSet] of io.sockets.adapter.rooms) {
+      if (!socketIds.has(room)) { 
+        rooms[room] = socketSet.size;
+      }
     }
-  }
 
-  return rooms;
-};
+    return rooms;
+  };
 });
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
-
